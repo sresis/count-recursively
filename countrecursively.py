f3ade987c4df9f24eebb89e7b49e3d5dc69abcb1
Stephanie Resis <sresis@umich.edu>
2020-09-21 23:32:15 -0500

    solve problem

--- countrecursively.py
+++ countrecursively.py
@@ -13,6 +13,10 @@ For example:
 
 def count_recursively(lst):
     """Return number of items in a list, using recursion."""
+    if not lst:
+        return 0
+    else:
+        return 1 + count_recursively(lst[1:])
 
 
 if __name__ == '__main__':
